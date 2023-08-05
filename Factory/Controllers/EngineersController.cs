cc742e37f189da3ec02b80cc4ff77f2ca5bd8bc4
sagepaden <sagepaden@gmail.com>
2023-08-05 14:44:46 -0700

    building out project before starting on intial databases

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+  public class EngineersController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public EngineersController(FactoryContext db)
+    {
+      _db = db;
+    }
+  }
+}
---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public int EngineerId { get; set; }
+
+    public string EngineerName { get; set; }
+
+    public int EngineerLicense { get; set; }
+
+    public int MachineId { get; set; }
+
+    public List<EngineerMachine> JoinEntities { get; set; }
+
+  }
+}
